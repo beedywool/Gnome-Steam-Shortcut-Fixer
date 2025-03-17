8a347552ffa2aa8ed057f50b16b598fe1c534a06
Beedy <beedywool@gmail.com>
2025-03-17 13:51:58 +0100

    Finish function to fix current shortcuts and improve game detection for new shortcuts creation

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # Gnome Steam Shortcut Fixer
 
-Simple utility to fix Steam shortcuts so the icon of the games is displayed correctly on GNOME instead of the default 'no icon' program.
-Tested only on GNOME 47 and Nobara 41.
+Simple utility to fix Steam shortcuts so the icon of the games RUNNING WITH PROTON is displayed correctly on GNOME instead of the default 'no icon' program.
+Tested only on GNOME 47 on Nobara 41 and Fedora 41.
 
 ## Features
 
-- Fix existing shortcuts to add `StartupWMClass`.
-- Create new shortcuts for all installed games in the SteamLibrary folder.
+- Fix existing shortcuts to add `StartupWMClass` pointing to the correct `steam_app_<appId>`.
+- Create new pre-patched shortcuts for all installed games in all SteamLibrary folders on the system
 
 ## Usage
 
@@ -16,16 +16,7 @@ Tested only on GNOME 47 and Nobara 41.
     cd GnomeSteamShortcutFixer
     ```
 
-2. Make sure `curl` and `jq` are installed on your system:  
-    Debian/Ubuntu
-    ```bash
-    sudo apt install curl jq
-    ```
-    Fedora
-    ```bash
-    sudo dnf install curl jq
-    ```
-
+2. Make sure `curl` and `jq` are installed on your system.
 
 3. Make the script executable:
     ```bash
--- gnome-steam-shortcut-fixer.sh
+++ gnome-steam-shortcut-fixer.sh
@@ -1,59 +1,132 @@
 #!/bin/bash
 
-# Variables
-shortcutsPath="$HOME/.local/share/applications"
-iconsPath="$HOME/.local/share/icons/hicolor/48x48/apps"
+
 
 # Functions
+# Function to init the variables
+initVariables() {
+    # Variables
+    shortcutsPath="$HOME/.local/share/applications"
+    iconsPath="$HOME/.local/share/icons/hicolor/"
+    steamLibraryConfigVdf="$HOME/.local/share/Steam/config/libraryfolders.vdf"
+    if [ ! -f "$steamLibraryConfigVdf" ]; then
+        # If the default path returns nothing try the flatpak path
+        echo -e "\e[31mSteam library config file not found in the default path. Trying the flatpak path\e[0m"
+        steamLibraryConfigVdf="$HOME/.var/app/com.valvesoftware.Steam/.local/share/Steam/config/libraryfolders.vdf"
+        if [ ! -f "$steamLibraryConfigVdf" ]; then
+            echo -e "\e[31mError: Steam library config file not found\e[0m"
+            exit 1
+        fi
+    fi
+    echo -e "\e[32mSteam library config file found at $steamLibraryConfigVdf\e[0m"
+}
+
 # Function to fix the existing shortcuts
 fixExistingShortcuts() {
     echo "Fixing existing shortcuts"
+    # Get all the .desktop files in the .local/share/applications folder
+    while IFS= read -r -d '' desktopFile; do
+        shortcutFiles+=("$desktopFile")
+    done < <(find "$shortcutsPath" -name "*.desktop" -print0)
+    echo "Found ${#shortcutFiles[@]} existing shortcuts"
+    # Loop through all the .desktop files and if one has an exec starting with steam
+    # Then get the ID from the exec and add it to the StartupWMClass
+    for shortcutFile in "${shortcutFiles[@]}"
+    do
+        # Get the exec line from the .desktop file
+        execLine=$(grep -oP '^Exec=.*' "$shortcutFile" | cut -d'=' -f2)
+        # Check if the exec line starts with steam
+        if [[ "$execLine" == "steam"* ]]; then
+            # Get the ID from the exec line
+            appId=$(echo "$execLine" | grep -oP '(?<=steam steam://rungameid/)[0-9]+')
+            if [ -z "$appId" ]; then
+                echo -e "\e[31mError: App ID not found in $shortcutFile\e[0m"
+                continue
+            else 
+                echo -e "\e[32mFixing $shortcutFile, with app id: $appId\e[0m"
+                # Check if the StartupWMClass already exists in the .desktop file
+                if grep -q "StartupWMClass" "$shortcutFile"; then
+                    echo -e "\e[90mStartupWMClass already exists, skipped\e[0m"
+                    echo -e "\e[90m--------------------------\e[0m"
+                else
+                    # Add the StartupWMClass to the .desktop file
+                    echo "StartupWMClass=steam_app_$appId" >> "$shortcutFile"
+                    echo -e "\e[32mStartupWMClass added to $shortcutFile\e[0m"
+                    echo -e "\e[90m--------------------------[0m"
+                fi
+            fi
+        fi
+    done
     # TODO
+}
+
+# Get all the installed appIds from the libraryfolders.vdf file
+getAllLibraryFolders() {
+    # Get the IDS
+    libraryFolders=($(    grep -oP '(?<="path"\t\t").*(?=")' "$steamLibraryConfigVdf"))
+    echo "Found ${#libraryFolders[@]} library folders"
+}
+
+# In the library folders get the installed apps ids from the appmanifest files
+getInstalledAppIds() {
+    # Loop through all the library folders
+    for libraryFolder in "${libraryFolders[@]}"
+    do
+        # Check if the steamapps folder exists in the library folder
+        if [ -d "$libraryFolder/steamapps" ]; then
+            echo -e "\e[32mFound steamapps folder in $libraryFolder\e[0m"
+            # Get the appmanifest files in the steamapps folder
+            appManifestFiles=($(find "$libraryFolder/steamapps" -name "appmanifest_*.acf"))
+            echo -e "\e[32mFound ${#appManifestFiles[@]} appmanifest files in $libraryFolder\e[0m"
+            # Loop through all the appmanifest files
+            for appManifestFile in "${appManifestFiles[@]}"
+            do
+                # Get the appid from the appmanifest file
+                appId=$(grep -oP '(?<="appid"\t\t").*(?=")' "$appManifestFile")
+                appIds+=("$appId")
+            done
+        else
+            echo -e "\e[31mError: steamapps folder not found in $libraryFolder\e[0m"
+        fi
+    done
 }
 
 # Function to create/replace new shortcuts for all games
 createNewShortcuts() {
-    # Look into the current folder for the steamapps/compatdata folder
-    # If it exists, then we are in the right folder, if it doesn't exist throw an error
-    if [ -d "steamapps/compatdata" ]; then
-        echo "Found steamapps/compatdata folder in the current directory"
-        # Get the name of all the folders in the steamapps/compatdata folder and store them in an array
-        appIds=($(ls steamapps/compatdata))
-        echo "Found ${#appIds[@]} games in the steamapps/compatdata folder"
-        # Loop through all the folders in the steamapps/compatdata folder
-        for appId in "${appIds[@]}"
-        do 
-            # Create a shortcut for each game in the steamapps/compatdata folder
-            # First, retrieve the name of the game from Steam API
-            gameName=$(curl -s "https://store.steampowered.com/api/appdetails?appids=$appId" | jq -r ".\"$appId\".data.name")
-            # If the game name is not null, then create a shortcut for the game in .local/share/applications
-            if [ "$gameName" != "null" ]; then
-                # Check if the icon exists in the .local/share/icons/hicolor/48x48/apps folder
-                gameIcon=$(ls "$iconsPath" | grep "steam_icon_$appId.png")
+    # Get the library folders from the libraryfolders.vdf file
+    getAllLibraryFolders
+    # Get all the installed appIds from the appmanifest files in the steamapps folder for all the library folders
+    getInstalledAppIds
+    # Loop through all the previously found appIds and create a shortcut for each game  
+    for appId in "${appIds[@]}"
+    do 
+        # Create a shortcut for each game in the steamapps/compatdata folder
+        # First, retrieve the name of the game from Steam API
+        gameName=$(curl -s "https://store.steampowered.com/api/appdetails?appids=$appId" | jq -r ".\"$appId\".data.name")
+        # If the game name is not null, then create a shortcut for the game in .local/share/applications
+        if [ "$gameName" != "null" ]; then
+            # Check if the icon exists in the .local/share/icons/hicolor/48x48/apps folder
+            gameIcon=$(find "$iconsPath" | grep "steam_icon_$appId.png")
 
-                echo "Creating shortcut for $gameName"
-                echo "[Desktop Entry]" > "$shortcutsPath/$gameName.desktop"
-                echo "Name=$gameName" >> "$shortcutsPath/$gameName.desktop"
-                echo "Exec=steam steam://rungameid/$appId" >> "$shortcutsPath/$gameName.desktop"
-                echo "Type=Application" >> "$shortcutsPath/$gameName.desktop"
-                # If the icon exists, then use it, otherwise use the default steam icon
-                if [ -n "$gameIcon" ]; then
-                    echo "Icon=steam_icon_$appId" >> "$shortcutsPath/$gameName.desktop"
-                else
-                    echo "Icon=steam" >> "$shortcutsPath/$gameName.desktop"
-                fi
-                echo "Categories=Game;" >> "$shortcutsPath/$gameName.desktop"
-                echo "Terminal=false" >> "$shortcutsPath/$gameName.desktop"
-                echo "StartupWMClass=steam_app_$appId" >> "$shortcutsPath/$gameName.desktop"
-                echo "Comment=Play $gameName on Steam" >> "$shortcutsPath/$gameName.desktop"
+            echo -e "\e[32mCreating shortcut for $gameName\e[0m"
+            echo "[Desktop Entry]" > "$shortcutsPath/$gameName.desktop"
+            echo "Name=$gameName" >> "$shortcutsPath/$gameName.desktop"
+            echo "Exec=steam steam://rungameid/$appId" >> "$shortcutsPath/$gameName.desktop"
+            echo "Type=Application" >> "$shortcutsPath/$gameName.desktop"
+            # If the icon exists, then use it, otherwise use the default steam icon
+            if [ -n "$gameIcon" ]; then
+                echo "Icon=steam_icon_$appId" >> "$shortcutsPath/$gameName.desktop"
             else
-                echo "Error: Game name not found for $folder"
+                echo "Icon=steam" >> "$shortcutsPath/$gameName.desktop"
             fi
-        done
-    else
-        echo "Error: steamapps/compatdata folder not found in the current directory"
-        exit 1
-    fi
+            echo "Categories=Game;" >> "$shortcutsPath/$gameName.desktop"
+            echo "Terminal=false" >> "$shortcutsPath/$gameName.desktop"
+            echo "StartupWMClass=steam_app_$appId" >> "$shortcutsPath/$gameName.desktop"
+            echo "Comment=Play $gameName on Steam" >> "$shortcutsPath/$gameName.desktop"
+        else
+            echo -e "\e[31mError: Name not found for $appId (it is probably not a game and doesn't need a shortcut)\e[0m"
+        fi
+    done
 }
 
 # Main
@@ -61,13 +134,13 @@ createNewShortcuts() {
 echo "Do you want to fix existing shortcuts or create new shortcuts for all the games installed in the current SteamLibrary directory?"
 echo "1. Fix existing shortcuts"
 echo "2. Create new shortcuts"
-read -p "Enter your choice (1 or 2): " REPLY
+read -p "Enter your choice (1 or 2): " userChoice
 
 # If the user chooses 1, then fix existing shortcuts
-if [ "$REPLY" == "1" ]; then
+if [ "$userChoice" == "1" ]; then
     echo "Fixing existing shortcuts"
     fixExistingShortcuts
-elif [ "$REPLY" == "2" ]; then
+elif [ "$userChoice" == "2" ]; then
     echo "Creating new shortcuts"
     createNewShortcuts
 else
