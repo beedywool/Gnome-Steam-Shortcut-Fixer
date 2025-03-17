2a605ce6f377722d5bf0e4db059c5855916f4f8a
Beedy <beedywool@gmail.com>
2025-03-17 19:26:12 +0100

    Update README and script to enhance shortcut functionality and usability

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
-# Gnome Steam Shortcut Fixer
+# Gnome Steam Shortcuts Utility
 
 Simple utility to fix Steam shortcuts so the icon of the games RUNNING WITH PROTON is displayed correctly on GNOME instead of the default 'no icon' program.
-Tested only on GNOME 47 on Nobara 41 and Fedora 41.
+Can also be used to generate shortcuts for ALL your installed Steam games at once.
+Note that the utility will fix the icon of games running with Proton and also apply this fix to native games, but the fix will only work with games running with Proton. For native games you can manually change the value of `StartupWMClass` in the .desktop file to the name of the executable. For exemple, for the game `Enter the Gungeon` you should put `StartupWMClass=EtG.x86_64`. Unfortunately this cannot be automated the same way as Proton games because it does not follow a pattern like them.
+
+Shortcut creation WILL overwrite existing Steam games shortcuts but will not change shortcuts unrelated to Steam.
+If the icon of the game is not found / doesn't exist it will default to the Steam icon.
+
+Utility tested on 2 PCs with GNOME 47 on Nobara 41 and Fedora 41.
 
 ## Features
 
-- Fix existing shortcuts to add `StartupWMClass` pointing to the correct `steam_app_<appId>`.
+- Fix existing shortcuts to add `StartupWMClass` pointing to the correct `steam_app_<appId>` to display the correctly display the icon of games running with Proton when they are opened.
 - Create new pre-patched shortcuts for all installed games in all SteamLibrary folders on the system
 
 ## Usage
@@ -20,15 +26,26 @@ Tested only on GNOME 47 on Nobara 41 and Fedora 41.
 
 3. Make the script executable:
     ```bash
-    chmod +x GnomeSteamShortcutFixer.sh
+    chmod +x ./gnome-steam-shortcut-fixer.sh
     ```
 
 4. Run the script:
     ```bash
-    ./GnomeSteamShortcutFixer.sh
+    ./gnome-steam-shortcut-fixer.sh
     ```
 
-5. Follow the prompts to either fix existing shortcuts or create new ones.
+5. Use the script from anywhere on your PC (it does not require to be executed from any specific directory).
+
+## Arguments
+
+Fix existing shortcuts:
+`-f` or `--fix`
+
+Create new shortcuts for all installed games:
+`-c` or `--create`
+
+Display the help message:
+`-h` or `--help`
 
 ## Requirements
 
--- gnome-steam-shortcut-fixer.sh
+++ gnome-steam-shortcut-fixer.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-
-
 # Functions
 # Function to init the variables
 initVariables() {
@@ -23,27 +21,35 @@ initVariables() {
 
 # Function to fix the existing shortcuts
 fixExistingShortcuts() {
-    echo "Fixing existing shortcuts"
+    echo -e "\e[90mFixing existing shortcuts\e[0m"
     # Get all the .desktop files in the .local/share/applications folder
     while IFS= read -r -d '' desktopFile; do
         shortcutFiles+=("$desktopFile")
     done < <(find "$shortcutsPath" -name "*.desktop" -print0)
-    echo "Found ${#shortcutFiles[@]} existing shortcuts"
+    echo -e "\e[90mFound ${#shortcutFiles[@]} existing shortcuts\e[0m"
+    echo -e "\e[90m--------------------------\e[0m"
     # Loop through all the .desktop files and if one has an exec starting with steam
     # Then get the ID from the exec and add it to the StartupWMClass
     for shortcutFile in "${shortcutFiles[@]}"
     do
         # Get the exec line from the .desktop file
+        gameName=$(grep -oP '^Name=.*' "$shortcutFile" | cut -d'=' -f2)
+        if [ "$gameName" == "Steam" ]; then
+            echo -e "\e[90mSkipping Steam shortcut\e[0m"
+            echo -e "\e[90m--------------------------\e[0m"
+            continue
+        fi
         execLine=$(grep -oP '^Exec=.*' "$shortcutFile" | cut -d'=' -f2)
         # Check if the exec line starts with steam
         if [[ "$execLine" == "steam"* ]]; then
             # Get the ID from the exec line
             appId=$(echo "$execLine" | grep -oP '(?<=steam steam://rungameid/)[0-9]+')
             if [ -z "$appId" ]; then
-                echo -e "\e[31mError: App ID not found in $shortcutFile\e[0m"
+                echo -e "\e[31mError: App ID not found for $gameName\e[0m"
+                echo -e "\e[90m--------------------------\e[0m"
                 continue
             else 
-                echo -e "\e[32mFixing $shortcutFile, with app id: $appId\e[0m"
+                echo -e "\e[32mFixing $gameName, with app id: $appId\e[0m"
                 # Check if the StartupWMClass already exists in the .desktop file
                 if grep -q "StartupWMClass" "$shortcutFile"; then
                     echo -e "\e[90mStartupWMClass already exists, skipped\e[0m"
@@ -52,7 +58,7 @@ fixExistingShortcuts() {
                     # Add the StartupWMClass to the .desktop file
                     echo "StartupWMClass=steam_app_$appId" >> "$shortcutFile"
                     echo -e "\e[32mStartupWMClass added to $shortcutFile\e[0m"
-                    echo -e "\e[90m--------------------------[0m"
+                    echo -e "\e[90m--------------------------\e[0m"
                 fi
             fi
         fi
@@ -64,7 +70,7 @@ fixExistingShortcuts() {
 getAllLibraryFolders() {
     # Get the IDS
     libraryFolders=($(    grep -oP '(?<="path"\t\t").*(?=")' "$steamLibraryConfigVdf"))
-    echo "Found ${#libraryFolders[@]} library folders"
+    echo -e "\e[90mFound ${#libraryFolders[@]} library folders\e[0m"
 }
 
 # In the library folders get the installed apps ids from the appmanifest files
@@ -74,10 +80,9 @@ getInstalledAppIds() {
     do
         # Check if the steamapps folder exists in the library folder
         if [ -d "$libraryFolder/steamapps" ]; then
-            echo -e "\e[32mFound steamapps folder in $libraryFolder\e[0m"
             # Get the appmanifest files in the steamapps folder
             appManifestFiles=($(find "$libraryFolder/steamapps" -name "appmanifest_*.acf"))
-            echo -e "\e[32mFound ${#appManifestFiles[@]} appmanifest files in $libraryFolder\e[0m"
+            echo -e "\e[90mFound ${#appManifestFiles[@]} appmanifest files in $libraryFolder\e[0m"
             # Loop through all the appmanifest files
             for appManifestFile in "${appManifestFiles[@]}"
             do
@@ -97,7 +102,8 @@ createNewShortcuts() {
     getAllLibraryFolders
     # Get all the installed appIds from the appmanifest files in the steamapps folder for all the library folders
     getInstalledAppIds
-    # Loop through all the previously found appIds and create a shortcut for each game  
+    # Loop through all the previously found appIds and create a shortcut for each game
+    echo -e "\e[90m--------------------------\e[0m"
     for appId in "${appIds[@]}"
     do 
         # Create a shortcut for each game in the steamapps/compatdata folder
@@ -109,6 +115,7 @@ createNewShortcuts() {
             gameIcon=$(find "$iconsPath" | grep "steam_icon_$appId.png")
 
             echo -e "\e[32mCreating shortcut for $gameName\e[0m"
+            echo -e "\e[90m--------------------------\e[0m"
             echo "[Desktop Entry]" > "$shortcutsPath/$gameName.desktop"
             echo "Name=$gameName" >> "$shortcutsPath/$gameName.desktop"
             echo "Exec=steam steam://rungameid/$appId" >> "$shortcutsPath/$gameName.desktop"
@@ -125,25 +132,41 @@ createNewShortcuts() {
             echo "Comment=Play $gameName on Steam" >> "$shortcutsPath/$gameName.desktop"
         else
             echo -e "\e[31mError: Name not found for $appId (it is probably not a game and doesn't need a shortcut)\e[0m"
+            echo -e "\e[90m--------------------------\e[0m"
         fi
     done
 }
 
-# Main
-# Ask if the user wants to fix existing shortcuts to add the StartupWMClass or create new shortcuts for all the games in the steamapps/compatdata folder
-echo "Do you want to fix existing shortcuts or create new shortcuts for all the games installed in the current SteamLibrary directory?"
-echo "1. Fix existing shortcuts"
-echo "2. Create new shortcuts"
-read -p "Enter your choice (1 or 2): " userChoice
+# Function to display the help message
+helpCommand() {
+    echo "Usage: gnome-steam-shortcut-fixer.sh [OPTION]"
+    echo "Fix or create new shortcuts for Steam games running with Proton on GNOME"
+    echo "Note: this utility will create and fix shortcuts even for native games not running with Proton, but the default icon won't be fixed for these games"
+    echo "Options:"
+    echo "  -h, --help      Display this help message"
+    echo "  -f, --fix       Fix existing shortcuts"
+    echo "  -c, --create    Create new shortcuts"
+}
 
-# If the user chooses 1, then fix existing shortcuts
-if [ "$userChoice" == "1" ]; then
-    echo "Fixing existing shortcuts"
-    fixExistingShortcuts
-elif [ "$userChoice" == "2" ]; then
-    echo "Creating new shortcuts"
-    createNewShortcuts
-else
-    echo "Invalid choice. Please enter 1 or 2"
-    exit 1
-fi
+# Main
+# Route the command line arguments
+case "$1" in
+    -h|--help)
+        helpCommand
+        exit 0
+        ;;
+    -f|--fix)
+        initVariables
+        fixExistingShortcuts
+        exit 0
+        ;;
+    -c|--create)
+        initVariables
+        createNewShortcuts
+        exit 0
+        ;;
+    *)
+        echo "Invalid option. Use -h or --help for help"
+        exit 1
+        ;;
+esac
